fdd9663b8e280c1839ac0b81f995f56bc89cdeb9
heyjanac <39075370+heyjanac@users.noreply.github.com>
2022-04-02 09:35:28 -0300

    commit inicial do projeto.

---
+++ src/main/java/com/espectra/relatorio/api/GeradorRelatorioPdfApplication.java
@@ -0,0 +1,13 @@
+package com.espectra.relatorio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorRelatorioPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorRelatorioPdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/espectra/relatorio/api/GeradorRelatorioPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espectra.relatorio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorRelatorioPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
